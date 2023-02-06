561ba5d6357dd6cf26dcd029e14914542d626db0
ardu1na <arduinadelbosque@gmail.com>
2023-02-06 12:46:54 -0300

    fixing expenses module models

--- expenses/models.py
+++ expenses/models.py
@@ -54,6 +54,10 @@ class Expense(models.Model):
 class Employee(models.Model):
     name = models.CharField(max_length=150, verbose_name="NAME")
     address = models.CharField(max_length=250, verbose_name="ADDRESS", blank=True, null=True)
+    email = models.EmailField(blank=True, null=True, verbose_name="EMAIL")
+    tel = models.CharField(max_length=40, blank=True, null=True, verbose_name="PHONE")
+    date_join = models.DateField(default=date.today)
+    active = models.BooleanField(default=True)
     
     def __str__ (self):
         return self.name
@@ -61,19 +65,54 @@ class Employee(models.Model):
     
 
     
+    
+    
+    
+    
 
 class Salary(models.Model):
-    NIGGA = 'Nigga'
-    WHITE = 'White'
+   
+    expense = models.OneToOneField(
+        Expense, on_delete=models.CASCADE,
+        blank=True, null=True)
 
-    WAGES_CHOICES = (
-        (NIGGA, ('Nigga')),
-        (WHITE, ('White')),
-        )
-
-    employee = models.ForeignKey(Employee, related_name="salarys", on_delete=models.CASCADE, verbose_name="EMPLOYEE")
-    kind = models.CharField(choices= WAGES_CHOICES, default='White', max_length=50, verbose_name="KIND")
-    expense = models.OneToOneField(Expense, on_delete=models.CASCADE, blank=True, null=True)
+    employee = models.ForeignKey(
+        Employee, related_name="salaries",
+        on_delete=models.CASCADE,
+        verbose_name="EMPLOYEE")
+    
+    white = models.DecimalField(
+        decimal_places=2, max_digits=15,
+        null=True, blank=True,
+        verbose_name="WHITE",
+        default=0)
+    
+    nigga = models.DecimalField(
+        decimal_places=2, max_digits=15,
+        null=True, blank=True,
+        verbose_name="NIGGA",
+        default=0)
+    
+    aguinaldo_mensual = models.DecimalField(
+        decimal_places=2, max_digits=15,
+        null=True,
+        blank=True,
+        verbose_name="AGUINALDO MENSUAL")
+    
+    
+    
+    def get_social(self):
+        return self.white/2
+    
+    def get_aguinaldo_mensual(self):
+        aguinaldo = self.white + self.nigga + self.get_social
+        return aguinaldo/12
+    
+    
+    def save(self, *args, **kwargs):
+        self.aguinaldo_mensual = self.get_aguinaldo_mensual
+        super(Salary, self).save(*args, **kwargs)
+        
 
     def __str__ (self):
-        return str(self.expense.value)
+        return '{} {}'.format(self.employee, self.expense.date)
