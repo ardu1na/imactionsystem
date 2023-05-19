9d7599259d1e6f9f27c3d8bd0d19330ac0a48fe3
ardu1na <arduinadelbosque@gmail.com>
2023-05-19 18:11:11 -0300

    changing logic in realting objects db service sale

--- sales/admin.py
+++ sales/admin.py
@@ -1,32 +1,29 @@
 
 from django.contrib import admin
 
-from unfold.admin import ModelAdmin
-from import_export.admin import ImportExportModelAdmin
 
-from dashboard.resources import SaleResource
+
 from sales.models import Sale, LastBlue, Service, Adj
+
+
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ['service', 'client', 'change', 'note']
+    search_fields = ['note',]
+admin.site.register(Sale, SaleAdmin)
 
 class SaleInline(admin.StackedInline):
     model = Sale
     extra = 0
 
 
-class ServiceAdmin(ModelAdmin):
+class ServiceAdmin(admin.ModelAdmin):
     inlines = [SaleInline,]
     extra= 0
 admin.site.register(Service, ServiceAdmin)
     
     
 
-class SaleAdmin(ModelAdmin, ImportExportModelAdmin):
-    list_display = ['service', 'client', 'change', 'note']
-    search_fields = ['note',]
-    resource_class = SaleResource
-   
-admin.site.register(Sale, SaleAdmin)
-
 admin.site.register(LastBlue)
-class AdjAdmin(ModelAdmin):
+class AdjAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Adj, AdjAdmin)
